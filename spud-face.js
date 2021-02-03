d06c8741c2e7a89952fd96046ebe3dee0f28a13f
Chuks <chuksota@gmail.com>
2021-02-03 16:43:32 -0600

    Phase two solved

--- spud-face.js
+++ spud-face.js
@@ -19,11 +19,29 @@ window.addEventListener("DOMContentLoaded", (event) => {
   });
 
   // ** Phase 2: Add focus and blur events to form inputs **
+  // let licenseForms = document.getElementsByClassName("form__input")
+  licenseInfo.addEventListener("focusin", event =>{
+    event.target.style.backgroundColor = "#90ee90"
+  })
 
-  
+  licenseInfo.addEventListener("focusout", event => {
+    event.target.style.backgroundColor = ""
+  })
 
   // ** Phase 3: Check that license numbers match **
 
+  const checkLicenseNumbers = event => {
+    const licenseValue = document.getElementById("license-num").value
+    const confirmLicValue = document.getElementById("license-num-confirm").value
+
+  if(licenseValue !== confirmLicValue){
+    event.preventDefault()
+    event.target.style.backgroundColor = "#ff7f50"
+  }
+  }
+
+
+  licenseInfo.addEventListener("submit", checkLicenseNumbers)
 
   // ** Phase 4: Update submit button click count **
 
